f440324f63343bf909677f3f077293c783220422
LGavin <ustbliugang@163.com>
2016-04-13 10:41:43 +0800

    Initial Commit

---
+++ 1111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1111
+//
+//  Created by Gavin on 16/4/13.
+//  Copyright © 2016年 Gavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1111
+//
+//  Created by Gavin on 16/4/13.
+//  Copyright © 2016年 Gavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
