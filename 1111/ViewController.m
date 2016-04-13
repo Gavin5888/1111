4f39e1c3255c8208a67232069b6b65d4db0a91aa
LGavin <ustbliugang@163.com>
2016-04-13 10:46:58 +0800

    init
    啊实打实的

--- 1111/ViewController.m
+++ 1111/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    啊实打实的
+    
+//    sad 
 }
 
 - (void)didReceiveMemoryWarning {
